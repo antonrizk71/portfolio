165f11ee3ae560a183cee92aebdf7493f12dc805
anton <antonrizk71@gmail.com>
2025-09-17 23:02:35 +0300

    first commit

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+// nav link active
+
+const links = document.querySelectorAll('#nav-links a');
+
+function setActiveLink() {
+    const currentHash = window.location.hash || '#home';
+    links.forEach(link => {
+        if (link.getAttribute('href') === currentHash) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+}
+
+
+setActiveLink();
+
+window.addEventListener('hashchange', setActiveLink);
